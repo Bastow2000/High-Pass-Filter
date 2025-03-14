1d940be877a022d12f377c7071edc33621d4c041
Bastow <77554338+Bastow2000@users.noreply.github.com>
2025-03-14 07:23:15 +0000

    Update DSP.c

--- DSP.c
+++ DSP.c
@@ -1,19 +1,12 @@
 #include "dsp.h"
 
-
 // ------- Function Setup  -------
-void setup(WavHeader *header, const float mainVol, const float cutoffFreq) {
-    // Filter both channels
-    FilterState state[NUM_CHANNELS] = {0};
-
+void setup(WavHeader *header, const int32_t *audioData) {
     // Setup wav format for a wav file
     setupWavFormat(header);
 
-    // Apply audio to memory, filter and set amplitude
-    setupAudioStream(header, state, mainVol, cutoffFreq);
-
-    // Write to the wave file
-    writeWavFile(header);
+    // Write the header file
+    writeWavFile(header, audioData);
 }
 
 void setupWavFormat(WavHeader *header) {
@@ -36,162 +29,152 @@ void setupWavFormat(WavHeader *header) {
     header->chunkSize = CHUNK_SIZE;
 }
 
-void setupAudioStream(WavHeader *header, FilterState *state, const float mainVol, const float cutoffFreq) {
-
-    // Reset filter state
-    filterReset(&state[0]);
-    filterReset(&state[1]);
-
-    header->data = (int32_t *) malloc(NUM_SAMPLES * NUM_CHANNELS * sizeof(int32_t));
-
+void setupAudioStream(WavHeader *header, int32_t *audioData, AudioState *audioState, const uint16_t packetCount) {
     // Calculated in an attempt to be efficient
-    const double sampleDuration = (double) NUM_SAMPLES / SAMPLE_RATE;
+    const float sampleDuration = (float) NUM_SAMPLES / SAMPLE_RATE;
 
-    double sineWave[NUM_SAMPLES];
-    double phaseIncrement[NUM_CHANNELS];
-    double phase[NUM_CHANNELS] = {0.0, 0.0};
+    for (u_int16_t sample = 0; sample < PACKET_SIZE; sample++) {
+        const uint16_t samples = packetCount + sample;
 
-    // Gets the lookup table for a sine wave
-    lookupSine(sineWave, mainVol);
+        for (u_int16_t channel = 0; channel < NUM_CHANNELS; channel++) {
+            // Choice for setting channel for audio
+            const float vol = setVolume(channel == 0 ? VOLUME1 : VOLUME2, &audioState->gainState[channel]);
+            const float freq = (channel == 0) ? FREQUENCY1 : FREQUENCY2;
 
-        for (u_int16_t sample = 0; sample < NUM_SAMPLES; sample++) {
+            audioState->phaseIncrement[channel] = sampleDuration * freq;
 
-            for (u_int16_t channel = 0; channel < NUM_CHANNELS; channel++) {
-                // Set state for each filter channel
-                setFilter(&state[channel], cutoffFreq);
+            // Interleave index
+            const u_int16_t interIndex = (samples * NUM_CHANNELS) + channel;
 
-                // Choice for setting channel for audio
-                const double vol = setVolume(channel == 0 ? VOLUME1 : VOLUME2);
-                const double freq = (channel == 0) ? FREQUENCY1 : FREQUENCY2;
+            // Linear interpolation
+            const u_int16_t phaseInt = (u_int16_t) audioState->phase[channel];
 
-                phaseIncrement[channel] = sampleDuration * freq;
+            const float phaseFrac = audioState->phase[channel] - (float) phaseInt;
 
-                // Interleave index
-                const u_int16_t interIndex = (sample * NUM_CHANNELS) + channel;
+            const float value = linearInterpolation(phaseFrac, phaseInt, audioState->sineWave);
 
-                // Linear interpolation
-                const u_int16_t phaseInt = (u_int16_t) phase[channel];
+            const float filteredValue = processFilter(&audioState->filterState[channel], (float) value);
 
-                const double phaseFrac = phase[channel] - phaseInt;
+            // Scale and store the data
+            audioData[interIndex] = (int32_t) ((vol * filteredValue) * INT32_MAX);
 
-                const double value = linearInterpolation(phaseFrac, phaseInt, sineWave );
-
-                const double filteredValue = processFilter(&state[channel], (float) value);
-
-
-                // Scale and store the data
-                header->data[interIndex] = (int32_t) ((vol * filteredValue) * INT32_MAX);
-
-                // Increment phase
-                phase[channel] += phaseIncrement[channel];
-                if (phase[channel] >= NUM_SAMPLES)
-                    phase[channel] -= NUM_SAMPLES;
-
+            // Increment phase
+            audioState->phase[channel] += audioState->phaseIncrement[channel];
+            if (audioState->phase[channel] >= NUM_SAMPLES)
+                audioState->phase[channel] -= NUM_SAMPLES;
         }
     }
 }
 
 // ------- Function Controls  -------
-double setVolume(const double gain) {
+float setVolume(const float gain, float *gainState) {
     // dBFS Conversion
-    static double gainState = 0;
 
     //5.9 for Volume 1 : 9.7 for volume 2
     // Linear Gain Formula: 10^(dB/20)
-    const double linearGain = (double) pow(10, gain / 20.0f);
+    const float linearGain = powf(10, gain / 20.0f);
 
     // is always 0 dBFS or lower
     // Needs normalisation
     //const double dbfGain = (double)(20 * log10 (linearGain / 1.0 ));
 
     // Logarithmic Interpolation formula: start *(end/start)^x
-    const double interpGain = gainState > 0
-                                  ? linearGain * pow(gainState / linearGain, VOLUME_INTERPOLATION)
-                                  : linearGain;
-    gainState = gain;
+    const float interpGain = *gainState > 0
+                                 ? linearGain * powf(*gainState / linearGain, VOLUME_INTERPOLATION)
+                                 : linearGain;
+    *gainState = linearGain;
 
     return interpGain;
 }
 
 
-void setFilter(FilterState *state, const float frequency) {
+void setFilter(FilterState *filterState, const float frequency) {
     // Normalise frequency
-    const double normFreq = 2 * M_PI * frequency / SAMPLE_RATE;
+    const float normFreq = 2.0f * (float) M_PI * frequency / SAMPLE_RATE;
 
     // Pre Warp
-    const double omegaC = 2 * SAMPLE_RATE * (tan(normFreq / 2));
+    const float omegaC = 2 * SAMPLE_RATE * (tanf(normFreq / 2));
 
     // First z coefficient for the denominator
-    const double z0 = pow(omegaC, 2);
+    const float z0 = powf(omegaC, 2);
 
     // Second z^{1} coefficient for the denominator
-    const double z1 = 135744 * omegaC;
+    const float z1 = 135744 * omegaC;
 
     // Third z^{2} coefficient for the denominator
-    const double z2 = pow(2 * SAMPLE_RATE, 2); //  9.216e10^{9} note for self
+    const float z2 = powf(2 * SAMPLE_RATE, 2); //  9.216e10^{9} note for self
 
     // the first z coefficient
-    const double norm = z0 + z1 + z2;
+    const float norm = z0 + z1 + z2;
 
-    const double a0 = z2 / norm;
+    const float a0 = z2 / norm;
 
-    state->a0 = a0;
-    state->a1 = (z2 * -2) / norm;
-    state->a2 = a0;
+    filterState->a0 = a0;
+    filterState->a1 = (z2 * -2) / norm;
+    filterState->a2 = a0;
 
-    state->b1 = (2.0 * z0 - 2.0 * z2) / norm;
-    state->b2 = (z0 - z1 + z2) / norm;
+    filterState->b1 = (2.0f * z0 - 2.0f * z2) / norm;
+    filterState->b2 = (z0 - z1 + z2) / norm;
 }
 
 // ------- Function Helpers  -------
-void lookupSine(double *sineWave, const float mainVol) {
+void lookupSine(float *sineWave, const float mainVol) {
     // Automatic Storage Duration - no memory clearing needed
     for (u_int16_t sample = 0; sample < NUM_SAMPLES; sample++) {
-        sineWave[sample] = mainVol * (sin(2.0f * M_PI * (float) sample / (float) NUM_SAMPLES));
+        sineWave[sample] = mainVol * (sinf(2.0f * (float) M_PI * (float) sample / (float) NUM_SAMPLES));
     }
 }
 
-double linearInterpolation(const double phaseFrac, const u_int16_t phaseInt,const double *sineTable) {
+float linearInterpolation(const float phaseFrac, const u_int16_t phaseInt, const float *sineTable) {
     const u_int16_t nextIndex = (phaseInt + 1) & (NUM_SAMPLES - 1);
-    const double currentSample = sineTable[phaseInt];
-    const double nextSample = sineTable[nextIndex];
-    const double sampleValue = currentSample + phaseFrac * (nextSample - currentSample);
+    const float currentSample = sineTable[phaseInt];
+    const float nextSample = sineTable[nextIndex];
+    const float sampleValue = currentSample + phaseFrac * (nextSample - currentSample);
 
     return sampleValue;
 }
 
-void filterReset(FilterState *state) {
-    state->inputX2 = 0;
-    state->inputX1 = 0;
-    state->outputY2 = 0;
-    state->outputY1 = 0;
+void filterReset(FilterState *filterState) {
+    filterState->inputX2 = 0;
+    filterState->inputX1 = 0;
+    filterState->outputY2 = 0;
+    filterState->outputY1 = 0;
 }
 
-double processFilter(FilterState *state, const float input){
-    const double output = state->a0 * input + state->a1 * state->inputX1 + state->a2 * state->inputX2
-                          - state->b1 * state->outputY1 - state->b2 * state->outputY2;
+float processFilter(FilterState *filterState, const float input) {
+    const float output = filterState->a0 * input + filterState->a1 * filterState->inputX1 + filterState->a2 *
+                         filterState->inputX2
+                         - filterState->b1 * filterState->outputY1 - filterState->b2 * filterState->outputY2;
 
 
-    state->inputX2 = state->inputX1;
-    state->inputX1 = input;
-    state->outputY2 = state->outputY1;
-    state->outputY1 = output;
+    filterState->inputX2 = filterState->inputX1;
+    filterState->inputX1 = input;
+    filterState->outputY2 = filterState->outputY1;
+    filterState->outputY1 = output;
     return output;
 }
 
-void writeWavFile(WavHeader *header) {
-
+void writeWavFile(const WavHeader *header, const int32_t *audioData) {
     FILE *file = fopen("output.wav", "wb");
-
 
     fwrite(header, sizeof(WavHeader), 1, file);
 
-    fwrite(header->data, sizeof(int32_t), NUM_SAMPLES * NUM_CHANNELS, file);
+    fwrite(audioData, sizeof(int32_t), NUM_SAMPLES * NUM_CHANNELS, file);
 
     fclose(file);
 
     system("afplay output.wav");
+}
 
-    // Free memory
-    free(header->data);
+void audioStateReset(AudioState *audioState) {
+    // Reset audio state before use
+    audioState->cutoffFreq = CUTOFF_FREQUENCY;
+    for (u_int16_t channel = 0; channel < NUM_CHANNELS; channel++) {
+        filterReset(&audioState->filterState[channel]);
+        audioState->phase[channel] = 0.0f;
+        audioState->gainState[channel] = 0.0f;
+    }
+    // Set state for each filter channel
+    setFilter(&audioState->filterState[0], audioState->cutoffFreq);
+    setFilter(&audioState->filterState[1], audioState->cutoffFreq);
 }
