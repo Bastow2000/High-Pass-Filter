da0bd857d2aa464dfb65f688d083a776aebbe900
Bastow <77554338+Bastow2000@users.noreply.github.com>
2025-03-14 07:24:51 +0000

    Update main.c

--- main.c
+++ main.c
@@ -4,10 +4,26 @@
 
 
 int main(void) {
-    WavHeader* header = malloc(sizeof(WavHeader));
+    // initialises space for the size of the header
+    WavHeader *header = malloc(sizeof(WavHeader));
+    AudioState *audioState = malloc(sizeof(AudioState));
+    int32_t *audioData = malloc(NUM_SAMPLES * NUM_CHANNELS * sizeof(int32_t));
+
+    // Resets filters, channels, phase and other
+    audioStateReset(audioState);
+
+    // Gets the lookup table for a sine wave
+    lookupSine(audioState->sineWave, MAIN_VOLUME);
+
+    // Process the data in packets
     for (u_int16_t packet = 0; packet < NUM_SAMPLES; packet += PACKET_SIZE) {
-        setup(header, MAIN_VOLUME, CUTOFF_FREQUENCY);
+        // Apply audio to memory, filter and set amplitude
+        setupAudioStream(header, audioData, audioState, packet);
     }
+    setup(header, audioData);
+
     free(header);
+    free(audioData);
+    free(audioState);
     return 0;
 }
